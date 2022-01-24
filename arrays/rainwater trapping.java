87ce8ee179ccea08009d5a0b1e0b0fdea29e15ec
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2022-01-25 03:18:41 +0530

    Update rainwater trapping.java

--- arrays/rainwater trapping.java
+++ arrays/rainwater trapping.java
@@ -1,1 +1,57 @@
 
+//gfg:
+class Solution{
+    
+    // arr: input array
+    // n: size of array
+    // Function to find the trapped water between the blocks.
+    static long trappingWater(int arr[], int n) { 
+        // Your code here
+        int[] left= new int[n];
+        int[] right=new int[n];
+        left[0] = arr[0];
+        
+        for(int i=1;i<n;i++){
+            left[i]= Math.max(left[i-1],arr[i]);
+        }
+        right[n-1]= arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            right[i] = Math.max(right[i+1],arr[i]);
+        }
+        long ans=0;
+        for(int i=0;i<n;i++){
+            ans+= (long)Math.min(left[i],right[i])-(long)arr[i];
+        }
+        return ans;
+    } 
+}
+
+//leetcode:
+class Solution {
+    public int trap(int[] height) {
+      int n = height.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        left[0] = height[0];
+        for(int i =1;i<n;i++){
+            left[i] = Math.max(left[i-1],height[i]);
+            
+        }
+        right[n-1] = height[n-1];
+        for(int i =n-2;i>=0;i--){
+            right[i] = Math.max(right[i+1] , height[i]);
+        }
+        int ans=0;
+        for(int i =1;i<n;i++){
+            ans+= Math.min(left[i],right[i])-height[i];
+        }
+        return ans;
+    }
+}
+/*Complexity Analysis: 
+Time Complexity: O(n). 
+Only one traversal of the array is needed, So time Complexity is O(n).
+
+Space Complexity: O(n). 
+Two extra arrays are needed each of size n.
+*/
