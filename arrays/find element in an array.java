1405a1da25f5917bc7ef564886a7b8a03bc1010c
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2022-02-03 00:09:13 +0530

    Update find element in an array.java

--- arrays/find element in an array.java
+++ arrays/find element in an array.java
@@ -1,31 +1,30 @@
-//span of array
 import java.io.*;
 import java.util.*;
 
 public class Main{
 
 public static void main(String[] args) throws Exception {
+  
     // write your code here
     Scanner sc= new Scanner(System.in);
-    int n =sc.nextInt();
-    int[] arr=new int[n];
+    int n= sc.nextInt();
+    int[] arr= new int[n];
     for(int i=0;i<arr.length;i++){
-        arr[i]=sc.nextInt();
+        arr[i] = sc.nextInt();
     }
-    int max=arr[0];
-    int min=arr[0];
-    for(int i=1;i<arr.length;i++){
-        if(arr[i] >max){
-            max=arr[i];
+    int data=sc.nextInt();
+    int idx=-1;
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]==data){
+            idx=i;
+            break;
         }
-        if(arr[i]<min){
-            min=arr[i];
-        }
-        
     }
-    int span=max-min;
-    System.out.println(span);
+    System.out.println(idx);
+    
     
     
     
  }
+
+}
