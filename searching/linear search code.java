de4d7e5b601390cfdcce4f199ab1b8977e3a9c01
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2022-02-14 02:39:05 +0530

    Update linear search code.java

--- searching/linear search code.java
+++ searching/linear search code.java
@@ -1,1 +1,19 @@
+//                           gfg
 
+class Solution {
+    public int search(int arr[], int n, int k) {
+
+        // code here
+        int temp=0;
+        for(int i=0;i<n;i++){
+            if(arr[i] ==k)
+            return i+1;//position
+            
+            
+        }
+        return -1;
+        
+        
+        
+    }
+}
