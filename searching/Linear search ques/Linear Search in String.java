96a5fe778098e4924f0e6b56a7bd3eab9662d794
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2022-07-19 10:55:37 +0530

    Update Linear Search in String.java

--- searching/Linear search ques/Linear Search in String.java
+++ searching/Linear search ques/Linear Search in String.java
@@ -1,1 +1,30 @@
+//string is an array of characters.
+// to go at every index of string we use str.charAt(i)
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String name="kunal";
+		char target='o';
+		System.out.println(search(name,target));
+
+	}
+	static boolean search(String str,char target){
+		if(str.length()==0){
+			return false;
+		}
+		for(int i =0;i<str.length();i++){
+            if(target==str.charAt(i)){
+				return true;
+			}
+		}
+		return false;
+	}
+}
 
