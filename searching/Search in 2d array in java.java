a3027cf747e0a4649ca5931f643f06a9370e5297
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2022-08-01 02:02:11 +0530

    Create Search in 2d array in java.java

---
+++ searching/Search in 2d array in java.java
@@ -0,0 +1,45 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* search in 2d arrays */
+class Main
+{
+	public static void main (String[] args)
+	{
+		
+        int[][] arr={
+                        {56,8,18},
+                        {4,6,7},
+                        {1,2,34}
+        };
+       int target=4;
+       int[] ans=search(arr,target);
+       System.out.println(Arrays.toString(ans));
+    }
+    static int[] search(int[][] arr,int target){
+        for(int row=0;row<arr.length;row++){
+            for(int col=0;col<arr[row].length;col++){
+                if(arr[row][col]==target){
+                    return new int[]{row,col};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+        
+    
+
