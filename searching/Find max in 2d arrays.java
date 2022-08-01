e200be9a45c7260ba1d6861b4031737e3603d474
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2022-08-02 02:42:32 +0530

    Update Find max in 2d arrays.java

--- searching/Find max in 2d arrays.java
+++ searching/Find max in 2d arrays.java
@@ -31,6 +31,45 @@ class Main
       return max;
     }
 }
+// using for each loop
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+//search in a 2d arrays
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        int[][] arr={
+            {1,2,3,4},
+            {5,6,7,9},
+            {4,5}
+        };
+        int ans=max(arr);
+        System.out.println(ans);
+
+    }
+    static int max(int[][] arr){
+        int max=Integer.MIN_VALUE;
+        for(int[] nums:arr){
+            
+            for(int element:nums){
+                if(element>max){
+                max=element;
+                }
+                
+
+            }
+            }
+        
+        return max;
+    }
+}
+
 
 
 
